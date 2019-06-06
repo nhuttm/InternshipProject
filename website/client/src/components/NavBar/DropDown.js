bde18e43ac9bc0bfd842993af9d033c61ed820db
Minh Nhut <1653061@student.hcmus.edu.vn>
2019-06-06 10:34:51 +0700

    ui navigation bar

--- website/client/src/components/NavBar/DropDown.js
+++ website/client/src/components/NavBar/DropDown.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./NavBar.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import DropDownList from './DropDownList';
 
 export default class DropDown extends React.Component{
 
@@ -14,9 +15,10 @@ export default class DropDown extends React.Component{
     render(){
         return(
             <React.Fragment>
-                <li className="dropdown">
+                <li className="dropdown-nav">
                     <a className="item-nav" href="#">{this.state.title}</a>
                     <button className="bttn-arrow-down"><FontAwesomeIcon icon="arrow-down"/></button>
+                    <DropDownList tops={this.props.tops} bottoms={this.props.bottoms} dresses={this.props.dresses} jackets={this.props.jackets} shoes={this.props.shoes} accesories={this.props.accesories} sales={this.props.sales}/>
                 </li>
             </React.Fragment>
         );
