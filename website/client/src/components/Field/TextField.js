0a250bf61a2d7f9354f18e987d362a53cb07b808
Minh Nhut <1653061@student.hcmus.edu.vn>
2019-06-10 00:47:37 +0700

    update modal

--- website/client/src/components/Field/TextField.js
+++ website/client/src/components/Field/TextField.js
@@ -6,7 +6,7 @@ export default class TextField extends React.Component {
         return (
             <React.Fragment>
                 <label className="title-label">{this.props.title}</label>
-                <input type='text' placeholder={this.props.placeholder} className="input-field"></input>
+                <input type='text' placeholder={this.props.placeholder} className="input-field" value={this.props.value} name={this.props.name} onChange={this.props.onChange}></input>
             </React.Fragment>
         )
     }
--- website/client/src/components/Modal/LoginModal.js
+++ website/client/src/components/Modal/LoginModal.js
@@ -8,9 +8,30 @@ import "./Modal.scss";
 
 export default class LoginModal extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            email: ' ',
+            pass: ' '
+        }
+    } 
 
-    handleLogin = () => {
+    handleLogin = (event) => {
+        event.preventDefault();
+        const form = {
+            name: this.state.name,
+            email: this.state.email
+           }
+        this.setState({
+            name: ' ',
+            email: ' '
+        })
+    }
 
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
 
     render() {
@@ -22,16 +43,16 @@ export default class LoginModal extends React.Component {
                         <Label title="Log In" className="login-label"/>
                     </div>
                     <div className="row">
-                        <TextField title='E-MAIL' placeholder='Enter your email' />
+                        <TextField title='E-MAIL' placeholder='Enter your email' name="email" value={this.state.email} onChange={e => this.handleChange(e)}/>
                     </div>
                     <div className="row">
-                        <TextField title='PASSWORD' placeholder='Enter your password' />
+                        <TextField title='PASSWORD' placeholder='Enter your password' name="pass" value={this.state.pass} onChange={e => this.handleChange(e)}/>
                     </div>
                     <div classNames="row">
                         <Checkbox label='Remember password' className="check-box"/>
                     </div>
                     <div className="row" style={{paddingTop: 20}}>
-                        <Button type="submit" className="bttn-login-sub" title="Login" onButtonClick={this.handleLogin} />
+                        <Button type="submit" className="bttn-login-sub" title="Login" onButtonClick={e => this.handleLogin(e)} />
                     </div>
                 </form>
                 <div className="line" style={{marginTop: 50}}></div>
